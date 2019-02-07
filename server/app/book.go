adca17804ad36aa4e8ebed7b68892879aba95466
gmidorii <stitow14@gmail.com>
2019-02-08 08:56:11 +0900

    Add books application

--- server/app/book.go
+++ server/app/book.go
@@ -1,6 +1,10 @@
 package app
 
-import "github.com/gmidorii/book/server/domain"
+import (
+	"github.com/gmidorii/book/server/domain"
+	"github.com/gmidorii/book/server/form"
+	"github.com/gmidorii/book/server/response"
+)
 
 type Book struct {
 	br domain.BookRepository
@@ -12,5 +16,25 @@ func NewBook(br domain.BookRepository) Book {
 	}
 }
 
-func (b Book) FetchList() []domain.Book {
+func (b Book) FetchList(form form.Books) (response.Books, error) {
+	books := br.GetList(form.Offset, form.Limit)
+
+	res := make([]response.Book, len(books))
+	for i, b := range books {
+		res[i] = response.Book{
+			ID:       b.ID,
+			Title:    b.Title,
+			Author:   b.Author,
+			Abstract: b.Abstract,
+			ImgURL:   b.Img.URL,
+		}
+	}
+
+	return response.Books{
+		Items: res,
+		Common: response.Common{
+			Count:  len(res),
+			Offset: form.Offset,
+		},
+	}, nil
 }
