9abea7c53838059e59894bc0af955a6bb3a5656a
gmidorii <stitow14@gmail.com>
2019-02-08 13:24:33 +0900

    Add get app struct from provider and return response

--- server/handler/book.go
+++ server/handler/book.go
@@ -5,14 +5,31 @@ import (
 	"net/http"
 
 	"github.com/gmidorii/book/server/form"
+	"github.com/gmidorii/book/server/provider"
 	"github.com/pkg/errors"
 )
 
 func Books(w http.ResponseWriter, r *http.Request) {
-	_, err := form.ParseBooks(r.URL.Query())
+	f, err := form.ParseBooks(r.URL.Query())
 	if err != nil {
 		log.Println(err)
 		errorMessage(w, http.StatusBadRequest, errors.Cause(err).Error())
 		return
 	}
+
+	app, err := provider.InitBookApp()
+	if err != nil {
+		log.Println(err)
+		errorMessage(w, http.StatusInternalServerError, "Server Error")
+		return
+	}
+
+	res, err := app.FetchList(f)
+	if err != nil {
+		log.Println(err)
+		errorMessage(w, http.StatusInternalServerError, "Server Error")
+		return
+	}
+
+	renderJSON(w, &res)
 }
